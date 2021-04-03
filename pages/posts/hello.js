1bc210adac00f33810b6c9aef01f0b3e3d4d1b1f
Kevin Weaver <kevinweaver2@gmail.com>
2021-04-03 16:38:13 -0400

    Add title to first post

--- pages/posts/hello.js
+++ pages/posts/hello.js
@@ -1,13 +1,25 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
 
 export default function Hello() {
   return (
     <>
+      <Head>
+        <title>Hello World</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
+        <Image
+          src="/images/kevin-mask.png" // Route of the image file
+          width={270} // Desired size with correct aspect ratio
+          height={310}
+          alt="Your Name"
+        />
       </h2>
     </>
   )
