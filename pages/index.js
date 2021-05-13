5de0ee47cd4393fd4e0750d35735a9becba30bc8
Kevin Weaver <kevinweaver2@gmail.com>
2021-05-12 23:46:12 -0400

    Add WIP timeline

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,24 @@
-import Head from 'next/head'
-import HomeHero from '../components/HomeHero.js'
+import Head from "next/head";
+import HomeHero from "../components/HomeHero.js";
+import Timeline from "../components/Timeline.js";
 
 export default function Home() {
   return (
-      <div className="bg-cover bg-center" style={{backgroundImage: 'url(images/background.png)'}}>
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Hi.</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div
+      className="bg-cover bg-center"
+      style={{ backgroundImage: "url(images/background.png)" }}
+    >
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <Head>
+          <title>Hi.</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <HomeHero/>
-      </main>
-
-    </div>
+        <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
+          <HomeHero />
+          <Timeline />
+        </main>
       </div>
-  )
+    </div>
+  );
 }
